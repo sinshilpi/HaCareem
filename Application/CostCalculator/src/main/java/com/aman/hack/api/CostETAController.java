e984dcadbeb05966c62b116db209c4a4b52f99ba
aman.singhal <aman.singhal@inmobi.com>
2017-02-26 03:29:47 +0530

    coat eta service combined

--- Application/CostCalculator/src/main/java/com/aman/hack/api/CostETAController.java
+++ Application/CostCalculator/src/main/java/com/aman/hack/api/CostETAController.java
@@ -1,7 +1,7 @@
 package com.aman.hack.api;
 
-import com.aman.hack.Service.CostServiceBean;
-import com.aman.hack.model.Cost;
+import com.aman.hack.Service.CostETAServiceBean;
+import com.aman.hack.model.CostETA;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -15,13 +15,13 @@ import org.springframework.web.bind.annotation.RestController;
  * Created by aman.singhal on 2/25/17.
  */
 @RestController
-public class CostController {
+public class CostETAController {
     @Autowired
-    private CostServiceBean costServiceBean;
-    @RequestMapping(value = "/api/findCost", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Cost> getItem(@RequestParam("length") long length, @RequestParam("breadth") long breadth,
+    private CostETAServiceBean costServiceBean;
+    @RequestMapping(value = "/api/findCostETA", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<CostETA> getItem(@RequestParam("length") long length, @RequestParam("breadth") long breadth,
             @RequestParam("height") long height, @RequestParam("distance") long distance) {
-        Cost cost = costServiceBean.findCost(length, breadth, height, distance);
-        return new ResponseEntity<Cost>(cost, HttpStatus.OK);
+        CostETA cost = costServiceBean.findCostETA(length, breadth, height, distance);
+        return new ResponseEntity<CostETA>(cost, HttpStatus.OK);
     }
 }
